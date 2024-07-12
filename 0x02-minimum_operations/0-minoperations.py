4827bb4f88074424d37d8a6557292748de392dbd
Darius Tohtin <dariustohtin@gmail.com>
2024-07-12 01:09:38 +0100

    task 0. Minimum Operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""defines a function minOperations(n)"""
+
+
+def minOperations(n):
+    """calculates the fewest number of operations needed to result in exactly 
+    n H characters in the file.
+    Args:
+        n(int): number of times the char 'H' must appear in the text file
+    Returns:
+        int: the minimum number of operations(Copy All and Paste) to get n * H
+    """
+    if n <= 1:
+        return 0
+    dp = [0] * (n + 1)
+
+    for i in range(2, n + 1):
+        dp[i] = float('inf')
+        for j in range(1, i // 2 + 1):
+            if i % j == 0:
+                dp[i] = min(dp[j], dp[j] + i // j)
+
+    return dp[n]
