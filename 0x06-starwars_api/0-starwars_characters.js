4bf859826d5825aab14e7eee214a6688e94bbaca
Darius Tohtin <dariustohtin@gmail.com>
2024-08-08 21:19:00 +0100

    used promises

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,10 +4,22 @@ const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
 request(url, { json: true }, (err, res, body) => {
   if (!err) {
-    body.characters.forEach(character => {
-      request(character, { json: true }, (err2, res2, body2) => {
-        if (!err2) { console.log(body2.name); }
+    const characterPromises = body.characters.map(character => {
+      return new Promise((resolve, reject) => {
+        request(character, { json: true }, (err2, res2, body2) => {
+          if (!err2) {
+            resolve(body2.name);
+          } else { reject(err2); }
+        });
       });
     });
+
+    Promise.all(characterPromises)
+      .then(names => {
+        names.forEach(name => console.log(name));
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 });
